ceaa6d9b8d127f8f5f17a84ee6ecc89822fecdd1
Pasha <redmi2.misan@gmail.com>
2021-02-03 18:41:32 +0200

    Updated table of status of all tables

--- resources/views/admin_panel/dashboards/ajax/generalTablesStatus.blade.php
+++ resources/views/admin_panel/dashboards/ajax/generalTablesStatus.blade.php
@@ -1,34 +1,51 @@
 @foreach($tables as $key => $table)
-    <tr class="{{ isset($table->order) && $table->getStatus() == 'Call' ? "call" : "" }}">
-        <td>{{ ++$key }}</td>
-        <td>{{ $table->number  }}</td>
 
-        @if(isset($table->order))
+    @if(isset($table->order))
 
-            @switch($table->getStatus())
-                @case('Call')
+        @switch($table->getStatus())
+            @case('Ordered')
+            <tr class="ordered">
+                <td>{{ ++$key }}</td>
+                <td>{{ $table->number  }}</td>
+                <td>
+                    <span class="mr-2"><span class="badge-dot badge-warning"></span>Ordered</span>
+                </td>
+                <td>{{ $table->getWaiterName()}}</td>
+                <td>{{ $table->order->created_at }}</td>
+            </tr>
+            @break
+
+            @case('Call')
+            <tr class="call">
+                <td>{{ ++$key }}</td>
+                <td>{{ $table->number  }}</td>
                 <td>
                     <span class="mr-2"><span class="badge-dot badge-primary"></span>Call</span>
                 </td>
                 <td>{{ $table->getWaiterName()}}</td>
                 <td>{{ $table->order->created_at }}</td>
-                @break
+            </tr>
+            @break
 
-                @default
+            @default
+            <tr class="">
+                <td>{{ ++$key }}</td>
+                <td>{{ $table->number  }}</td>
                 <td>
                     <span class="mr-2"><span class="badge-dot badge-success"></span>Open</span>
                 </td>
                 <td>{{ $table->getWaiterName()}}</td>
                 <td>{{ $table->order->created_at }}</td>
-            @endswitch
+            </tr>
+        @endswitch
 
-        @else
-
+    @else
+        <tr class="">
+            <td>{{ ++$key }}</td>
+            <td>{{ $table->number  }}</td>
             <td><span class="mr-2"><span class="badge-dot badge-light"></span>Free</span></td>
             <td>-</td>
             <td>-</td>
-
-        @endif
-
-    </tr>
+        </tr>
+    @endif
 @endforeach
